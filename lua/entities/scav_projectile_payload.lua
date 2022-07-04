d683a7c1f12f22aee32a1d2dd83c07ea52b0689d
Sierra Foxtrot <sagejfox@aol.com>
2022-07-04 18:35:02 -0500

    Hyper Stamina Payload
    Added default prop for Hyper Beam.
    Added stamina adjusting functions to Speed and Slow statuses for when gmod_suit is enabled.
    Fixed flak shell not rendering when fired as Payload Gun, as well as added additional missle models. Added non-TF2 effects for non-TF2 ammunition.

--- lua/entities/scav_projectile_payload.lua
+++ lua/entities/scav_projectile_payload.lua
@@ -8,20 +8,32 @@ ENT.Contact = "none"
 ENT.Purpose = "none"
 ENT.Instructions = "DON'T FUCKING SPAWN THIS SHIT I'M SERIOUS"
 ENT.dettime = 2
+ENT.tf2 = false
 
 function ENT:Initialize()
-	self.Entity:PhysicsInitBox(Vector(-24,-24,-24),Vector(24,24,24))
+	self.Entity:PhysicsInit(SOLID_VPHYSICS)
+	if not self.Entity:GetPhysicsObject():IsValid() then
+		self.Entity:PhysicsInitBox(Vector(-24,-24,-24),Vector(24,24,24))
+		self.Entity:SetSolid(SOLID_VPHYSICS)
+	end
 	self.Entity:SetMoveType(MOVETYPE_VPHYSICS)
-	self.Entity:SetSolid(SOLID_VPHYSICS)
 	
+	if self.Entity:GetModel() == "models/props_trainyard/cart_bomb_separate.mdl" then
+		self.tf2 = true
+	end
+
 	if SERVER then
 		for k,v in ipairs(ents.GetAll()) do
 			if v:IsNPC() then
-				v:AddEntityRelationship(self,2,99)
+				v:AddEntityRelationship(self,D_FR,99)
 			end
 		end
 	else
-		self:EmitSound("items/cart_explode_trigger.wav")
+		if self.tf2 then
+			self:EmitSound("items/cart_explode_trigger.wav")
+		else
+			self:EmitSound("ambient/explosions/explode_4.wav",75,100,.75)
+		end
 	end
 end
 
@@ -29,7 +41,13 @@ function ENT:Use()
 end
 
 function ENT:PhysicsUpdate()
-	self:SetLocalAngles((self:GetPhysicsObject():GetVelocity()*-1):Angle())
+	local ang = self:GetPhysicsObject():GetVelocity():Angle()
+	if self.Entity:GetModel() == "models/props_phx/misc/flakshell_big.mdl" then
+		ang:Add(Angle(90,0,0))
+	elseif self.Entity:GetModel() == "models/props_trainyard/cart_bomb_separate.mdl" then
+		ang:Add(Angle(180,0,0))
+	end
+	self:SetLocalAngles(ang)
 end
 
 function ENT:OnTakeDamage()
@@ -52,6 +70,10 @@ if CLIENT then
 		self:DrawModel()
 	end
 
+	function ENT:DrawTranslucent()
+		self:DrawModel()
+	end
+
 	function ENT:Think()
 	end
 end
@@ -70,14 +92,22 @@ if SERVER then
 				local rf = RecipientFilter()
 				rf:AddAllPlayers()
 				net.WriteVector(self:GetPos())
-				net.WriteString("items/cart_explode.wav")
+				if self.tf2 then
+					net.WriteString("items/cart_explode.wav")
+				else
+					net.WriteString("ambient/explosions/explode_5.wav")
+				end
 			net.Send(rf)
 
 			self:SetPos(self:GetPos() + vector_up * 200)
 			self:SetLocalAngles((vector_up * -1):Angle())
 			util.ScreenShake(self:GetPos(),16,50,1,5000)
 			util.BlastDamage(self,self.Owner,self:GetPos(),1000,500)
-			ParticleEffectAttach("cinefx_goldrush",PATTACH_ABSORIGIN_FOLLOW,self,0)
+			if self.tf2 then
+				ParticleEffectAttach("cinefx_goldrush",PATTACH_ABSORIGIN_FOLLOW,self,0)
+			else
+				ParticleEffectAttach("scav_exp_fireball3",PATTACH_ABSORIGIN_FOLLOW,self,0)
+			end
 			self:Fire("kill",1,10)
 			self:SetNoDraw(true)
 			self:DrawShadow(false)
